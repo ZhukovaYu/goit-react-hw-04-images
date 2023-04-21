f8ae772b5ebfbeb6df1a4032ed6e47f40841fa03
ZhukovaYu <zhukova.julia@gmail.com>
2023-04-21 20:12:14 +0200

    goit-react-hw-04-images

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import style from './Modal.module.css';
 
 export const Modal = ({ largeImageURL, onClose }) => {
-  const handleKeyDown = (event) => {
-    if (event.code === 'Escape') {
-      onClose();
-    }
-  };
-
   const handleBackdropClick = (event) => {
     if (event.currentTarget === event.target) {
       onClose();
@@ -15,11 +9,16 @@ export const Modal = ({ largeImageURL, onClose }) => {
   };
 
   useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
     window.addEventListener('keydown', handleKeyDown);
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [handleKeyDown]);
+  }, [onClose]);
 
   return (
     <div className={style.overlay} onClick={handleBackdropClick}>
