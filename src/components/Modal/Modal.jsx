74629cfdacd8862bbdd7bd0bd3657499161551b3
ZhukovaYu <zhukova.julia@gmail.com>
2023-04-22 10:34:26 +0200

    update Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect } from 'react';
 import style from './Modal.module.css';
 
 export const Modal = ({ largeImageURL, onClose }) => {
